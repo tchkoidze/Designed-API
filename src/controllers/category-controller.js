3ba79b9ae0a3876c34b3964ba90d71f8013c31ae
unknown <ilia.chkoidze12@gmail.com>
2023-05-07 12:25:53 +0400

    refactor

--- src/controllers/category-controller.js
+++ src/controllers/category-controller.js
@@ -2,6 +2,12 @@ import Category from "../models/category.js";
 
 export const getAllCategories = async (req, res) => {
   const data = await Category.find();
+  const newData = data.map((element) => {
+    return {
+      id: element.id,
+      name: element.name,
+    };
+  });
 
   return res.status(200).json(data);
 };
